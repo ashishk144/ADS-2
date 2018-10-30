8d2a2fcb1c5b0352097cb4e7b7275cf16ec13dff
ashishk144 <ashishk1404@msitprogram.net>
2018-10-30 09:52:54 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2 Assignments/M1/1.1 Graph/Solution.java
+++ ADS2 Assignments/M1/1.1 Graph/Solution.java
@@ -1,11 +1,45 @@
 import java.util.Scanner;
 
+/**
+ * Interface for graph.
+ */
 interface Graph {
-    public int V();
-    public int E();
-    public void addEdge(int v, int w);
-    public Iterable<Integer> adj(int v);
-    public boolean hasEdge(int v, int w);
+    /**
+     * number of vertices.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int vertices();
+    /**
+     * number of edges.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int edges();
+    /**
+     * Adds an edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     */
+    void addEdge(int v, int w);
+    /**
+     * iterable.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    Iterable<Integer> adj(int v);
+    /**
+     * Determines if it has edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    boolean hasEdge(int v, int w);
 }
 
 /**
@@ -41,7 +75,7 @@ public class Solution {
             }
             System.out.println(g.display(input));
         } else {
-            AdjMatrixGraph mat = new AdjMatrixGraph(vertices);
+            GraphMatrix mat = new GraphMatrix(vertices);
             for (int i = 0; i < edges; i++) {
                 String[] add = scan.nextLine().split(" ");
                 int a = Integer.parseInt(add[0]);
@@ -50,7 +84,7 @@ public class Solution {
                     mat.addEdge(a, b);
                 }
             }
-            System.out.println(mat.display());
+            System.out.println(mat);
         }
     }
 }
