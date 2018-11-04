ff40033bc0fd43339c237719109b06671136d25b
ashishk144 <ashishk1404@msitprogram.net>
2018-11-04 22:43:07 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS2 Assignments/M4/Code Camp - WordNet/WordNet/SAP.java
+++ ADS2 Assignments/M4/Code Camp - WordNet/WordNet/SAP.java
@@ -1,23 +1,50 @@
-import java.util.*;
+import java.util.HashSet;
 /**
  * Class for sap.
  */
 public class SAP {
-    Digraph graph;
-    Integer dist;
-    Integer ancestor;
+    /**
+     * { var_description }.
+     */
+    private Digraph graph;
+    /**
+     * { var_description }.
+     */
+    private Integer dist;
+    /**
+     * { var_description }.
+     */
+    private Integer ancestor;
     // constructor takes a digraph (not necessarily a DAG)
-    public SAP(Digraph G) {
+
+    /**
+     * Constructs the object.
+     *
+     * @param      G     { parameter_description }
+     */
+    public SAP(final Digraph G) {
         this.graph = G;
         dist = Integer.MAX_VALUE;
     }
 
-    // length of shortest ancestral path between v and w; -1 if no such path
-    public int length(int v, int w) {
-        BreadthFirstDirectedPaths bfs1 = new BreadthFirstDirectedPaths(this.graph, v);
-        BreadthFirstDirectedPaths bfs2 = new BreadthFirstDirectedPaths(this.graph, w);
-        Set<Integer> s1 = new HashSet<Integer>();
-        Set<Integer> s2 = new HashSet<Integer>();
+    // length of shortest ancestral path between v
+    // and w; -1 if no such path
+
+    /**
+     * { function_description }
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int length(final int v, final int w) {
+        BreadthFirstDirectedPaths bfs1 = new BreadthFirstDirectedPaths(
+            this.graph, v);
+        BreadthFirstDirectedPaths bfs2 = new BreadthFirstDirectedPaths(
+            this.graph, w);
+        HashSet<Integer> s1 = new HashSet<Integer>();
+        HashSet<Integer> s2 = new HashSet<Integer>();
         for (int i = 0; i < this.graph.vertices(); i++) {
             if (bfs1.hasPathTo(i)) {
                 s1.add(i);
@@ -39,12 +66,24 @@ public class SAP {
         return dist;
     }
 
-    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    public int ancestor(int v, int w) {
-        BreadthFirstDirectedPaths bfs1 = new BreadthFirstDirectedPaths(this.graph, v);
-        BreadthFirstDirectedPaths bfs2 = new BreadthFirstDirectedPaths(this.graph, w);
-        Set<Integer> s1 = new HashSet<Integer>();
-        Set<Integer> s2 = new HashSet<Integer>();
+    // a common ancestor of v and w that participates in a shortest
+    //ancestral path; -1 if no such path
+
+    /**
+     * { function_description }.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int ancestor(final int v, final int w) {
+        BreadthFirstDirectedPaths bfs1 = new BreadthFirstDirectedPaths(
+            this.graph, v);
+        BreadthFirstDirectedPaths bfs2 = new BreadthFirstDirectedPaths(
+            this.graph, w);
+        HashSet<Integer> s1 = new HashSet<Integer>();
+        HashSet<Integer> s2 = new HashSet<Integer>();
         for (int i = 0; i < this.graph.vertices(); i++) {
             if (bfs1.hasPathTo(i)) {
                 s1.add(i);
@@ -68,7 +107,17 @@ public class SAP {
 
     // length of shortest ancestral path between any vertex in v and
     // any vertex in w; -1 if no such path
-    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+
+    /**
+     * { function_description }.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int length(final Iterable<Integer> v,
+        final Iterable<Integer> w) {
         for (int i : v) {
             for (int j : w) {
                 int tdist = length(i, j);
@@ -84,7 +133,17 @@ public class SAP {
         return dist;
     }
 
-    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    // a common ancestor that participates in shortest
+    // ancestral path; -1 if no such path
+
+    /**
+     * { function_description }.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
         for (int i : v) {
             for (int j : w) {
