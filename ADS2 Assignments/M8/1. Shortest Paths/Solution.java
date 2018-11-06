aefaea930fe6f11bf6595a1e09d49fba33fb58c9
ashishk144 <ashishk1404@msitprogram.net>
2018-11-07 00:08:15 +0530

    "ads2_m8_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ ADS2 Assignments/M8/1. Shortest Paths/Solution.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * Class for solution.
+ */
+public class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * Main file to handle inputs and deliver outputs.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] v = scan.nextLine().split(" ");
+        EdgeWeightedGraph ewg = new EdgeWeightedGraph(Integer.parseInt(v[0]));
+        HashMap<String, Integer> ids = new HashMap<String, Integer>();
+        String[] inp = scan.nextLine().split(" ");
+        for(int i = 0; i < inp.length; i++) {
+            ids.put(inp[i], i);
+        }
+        for (int i = 0; i < Integer.parseInt(v[1]); i++) {
+            inp = scan.nextLine().split(" ");
+            Edge e = new Edge(ids.get(inp[0]), ids.get(inp[1]),
+                Double.parseDouble(inp[2]));
+            ewg.addEdge(e);
+        }
+        int iter = Integer.parseInt(scan.nextLine());
+        inp = scan.nextLine().split(" ");
+        for (int i = 0; i < iter; i++) {
+            DijkstraUndirectedSP djsp = new DijkstraUndirectedSP(ewg,
+                ids.get(inp[0]));
+            System.out.println(djsp.distTo(ids.get(inp[1])));
+        }
+
+    }
+}
