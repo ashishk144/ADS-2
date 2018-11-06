8f16952fa41c4da28d9ed9224b5a08db8078937f
ashishk144 <ashishk1404@msitprogram.net>
2018-11-06 17:34:30 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2 Assignments/M7/1. MST/MinPQ.java
+++ ADS2 Assignments/M7/1. MST/MinPQ.java
@@ -1,22 +1,30 @@
 import java.util.Comparator;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.ArrayList;
 /**
  * Class for minimum pq.
  *
  * @param      <Key>  The key
  */
 public class MinPQ<Key> {
-    private Key[] pq;                    // store items at indices 1 to n
-    private int n;                       // number of items on priority queue
-    private Comparator<Key> comparator;  // optional comparator
+    /**
+     * stores items from 1 to n.
+     */
+    private Key[] pq;
+    /**
+     * Number of items on priority queue.
+     */
+    private int n;
+    /**
+     * Comparator variable.
+     */
+    private Comparator<Key> comparator;
 
     /**
      * Initializes an empty priority queue with the given initial capacity.
      *
      * @param  initCapacity the initial capacity of this priority queue
      */
-    public MinPQ(int initCapacity) {
+    public MinPQ(final int initCapacity) {
         pq = (Key[]) new Object[initCapacity + 1];
         n = 0;
     }
@@ -27,50 +35,10 @@ public class MinPQ<Key> {
     public MinPQ() {
         this(1);
     }
-
     /**
-     * Initializes an empty priority queue with the given initial capacity,
-     * using the given comparator.
+     * Determines if empty.
      *
-     * @param  initCapacity the initial capacity of this priority queue
-     * @param  comparator the order in which to compare the keys
-     */
-    public MinPQ(int initCapacity, Comparator<Key> comparator) {
-        this.comparator = comparator;
-        pq = (Key[]) new Object[initCapacity + 1];
-        n = 0;
-    }
-
-    /**
-     * Initializes an empty priority queue using the given comparator.
-     *
-     * @param  comparator the order in which to compare the keys
-     */
-    public MinPQ(Comparator<Key> comparator) {
-        this(1, comparator);
-    }
-
-    /**
-     * Initializes a priority queue from the array of keys.
-     * <p>
-     * Takes time proportional to the number of keys, using sink-based heap construction.
-     *
-     * @param  keys the array of keys
-     */
-    public MinPQ(Key[] keys) {
-        n = keys.length;
-        pq = (Key[]) new Object[keys.length + 1];
-        for (int i = 0; i < n; i++)
-            pq[i+1] = keys[i];
-        for (int k = n/2; k >= 1; k--)
-            sink(k);
-    }
-
-    /**
-     * Returns true if this priority queue is empty.
-     *
-     * @return {@code true} if this priority queue is empty;
-     *         {@code false} otherwise
+     * @return     True if empty, False otherwise.
      */
     public boolean isEmpty() {
         return n == 0;
@@ -89,15 +57,16 @@ public class MinPQ<Key> {
      * Returns a smallest key on this priority queue.
      *
      * @return a smallest key on this priority queue
-     * @throws NoSuchElementException if this priority queue is empty
      */
     public Key min() {
-        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
         return pq[1];
     }
-
-    // helper function to double the size of the heap array
-    private void resize(int capacity) {
+    /**
+     * Resizing function.
+     *
+     * @param      capacity  The capacity
+     */
+    private void resize(final int capacity) {
         Key[] temp = (Key[]) new Object[capacity];
         for (int i = 1; i <= n; i++) {
             temp[i] = pq[i];
@@ -110,11 +79,10 @@ public class MinPQ<Key> {
      *
      * @param  x the key to add to this priority queue
      */
-    public void insert(Key x) {
-        // double size of array if necessary
-        if (n == pq.length - 1) resize(2 * pq.length);
-
-        // add x, and percolate it up to maintain heap invariant
+    public void insert(final Key x) {
+        if (n == pq.length - 1) {
+            resize(2 * pq.length);
+        }
         pq[++n] = x;
         swim(n);
     }
@@ -123,55 +91,89 @@ public class MinPQ<Key> {
      * Removes and returns a smallest key on this priority queue.
      *
      * @return a smallest key on this priority queue
-     * @throws NoSuchElementException if this priority queue is empty
      */
     public Key delMin() {
-        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        final int four = 4;
         Key min = pq[1];
         exch(1, n--);
         sink(1);
-        pq[n+1] = null;     // to avoid loiterig and help with garbage collection
-        if ((n > 0) && (n == (pq.length - 1) / 4)) resize(pq.length / 2);
+        pq[n + 1] = null;
+        if ((n > 0) && (n == (pq.length - 1) / four)) {
+            resize(pq.length / 2);
+        }
         return min;
     }
-
-
-   /***************************************************************************
-    * Helper functions to restore the heap invariant.
-    ***************************************************************************/
-
-    private void swim(int k) {
-        while (k > 1 && greater(k/2, k)) {
-            exch(k, k/2);
-            k = k/2;
+    /**
+    * Swim function.
+    *
+    * @param      k1     { parameter_description }
+    */
+    private void swim(final int k1) {
+        int k = k1;
+        while (k > 1 && greater(k / 2, k)) {
+            exch(k, k / 2);
+            k = k / 2;
         }
     }
-
-    private void sink(int k) {
-        while (2*k <= n) {
-            int j = 2*k;
-            if (j < n && greater(j, j+1)) j++;
-            if (!greater(k, j)) break;
+    /**
+     * Sink function.
+     *
+     * @param      k1     { parameter_description }
+     */
+    private void sink(final int k1) {
+        int k = k1;
+        while (2 * k <= n) {
+            int j = 2 * k;
+            if (j < n && greater(j, j + 1)) {
+                j++;
+            }
+            if (!greater(k, j)) {
+                break;
+            }
             exch(k, j);
             k = j;
         }
     }
-
-   /***************************************************************************
-    * Helper functions for compares and swaps.
-    ***************************************************************************/
-    private boolean greater(int i, int j) {
+    /**
+     * Checks if greater.
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private boolean greater(final int i, final int j) {
         if (comparator == null) {
             return ((Comparable<Key>) pq[i]).compareTo(pq[j]) > 0;
-        }
-        else {
+        } else {
             return comparator.compare(pq[i], pq[j]) > 0;
         }
     }
-
-    private void exch(int i, int j) {
+    /**
+     * Exchange function.
+     *
+     * @param      i     { parameter_description }
+     * @param      j1     { parameter_description }
+     */
+    private void exch(final int i, final int j1) {
+        int j = j1;
         Key swap = pq[i];
         pq[i] = pq[j];
         pq[j] = swap;
+    }
+
+    /**
+     * Prints the minimum 5 of the PQ.
+     *
+     * @param      worst  The worst
+     */
+    public void print5(final ArrayList<Key> worst) {
+        final int five = 5;
+        for (int i = 1; i <= five; i++) {
+            Key temp = delMin();
+            System.out.println(temp);
+            worst.add(temp);
+        }
+        System.out.print("\n");
     }
 }
