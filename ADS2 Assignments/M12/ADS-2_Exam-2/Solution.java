a6bb22ebb7d19dcc05e3d64346be32fbc3503921
ashishk144 <ashishk1404@msitprogram.net>
2018-11-10 10:52:38 +0530

    "ads2_m12_a1_1: 1/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS2 Assignments/M12/ADS-2_Exam-2/Solution.java
+++ ADS2 Assignments/M12/ADS-2_Exam-2/Solution.java
@@ -21,40 +21,59 @@ public class Solution {
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
         int k = Integer.parseInt(scan.nextLine());
-        Edge e;
-        EdgeWeightedGraph ewg = new EdgeWeightedGraph(n);
+        String[] inp;
+        DirectedEdge e;
+        EdgeWeightedDigraph ewg = new EdgeWeightedDigraph(n);
         for (int i = 0; i < k; i++) {
-            String[] inp = scan.nextLine().split(" ");
+            inp = scan.nextLine().split(" ");
             if (!inp[0].equals(inp[1])) {
-                e = new Edge(Integer.parseInt(inp[0]), Integer.parseInt(inp[1]),
+                e = new DirectedEdge(Integer.parseInt(inp[0]),
+                             Integer.parseInt(inp[1]),
                              Integer.parseInt(inp[2]));
                 ewg.addEdge(e);
             }
         }
         String caseToGo = scan.nextLine();
-        switch (caseToGo) {
-        case "Graph":
-            //Print the Graph Object.
-            System.out.println(ewg);
-            break;
+        try {
+            switch (caseToGo) {
+            case "Graph":
+                //Print the Graph Object.
+                System.out.println(ewg);
+                break;
 
-        case "DirectedPaths":
-            // Handle the case of DirectedPaths, where two integers are given.
-            // First is the source and second is the destination.
-            // If the path exists print the distance between them.
-            // Other wise print "No Path Found."
-            break;
+            case "DirectedPaths":
+                // Handle the case of DirectedPaths, where two integers are
+                // given.
+                // First is the source and second is the destination.
+                // If the path exists print the distance between them.
+                // Other wise print "No Path Found."
+                DijkstraSP dj;
+                while (scan.hasNextLine()) {
+                    inp = scan.nextLine().split(" ");
+                    dj = new DijkstraSP(ewg, Integer.parseInt(inp[0]));
+                    if (dj.hasPathTo(Integer.parseInt(inp[1]))) {
+                        System.out.println(dj.distTo(Integer.parseInt(
+                                                         inp[1])));
+                    } else {
+                        throw new Exception("No Path Found.");
+                    }
+                }
+                break;
 
-        case "ViaPaths":
-            // Handle the case of ViaPaths, where three integers are given.
-            // First is the source and second is the via is the one where path should pass throuh.
-            // third is the destination.
-            // If the path exists print the distance between them.
-            // Other wise print "No Path Found."
-            break;
+            case "ViaPaths":
+                // Handle the case of ViaPaths, where three integers are given.
+                // First is the source and second is the via is the one where
+                // path should pass throuh.
+                // third is the destination.
+                // If the path exists print the distance between them.
+                // Other wise print "No Path Found."
+                break;
 
-        default:
-            break;
+            default:
+                break;
+            }
+        } catch (Exception exp) {
+            System.out.println(exp.getMessage());
         }
 
     }
