cc245653867f2376c6920e9791913a834f686d4e
ashishk144 <ashishk1404@msitprogram.net>
2018-11-12 18:19:21 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2 Assignments/M13/Radix Sorts/Quick3string.java
+++ ADS2 Assignments/M13/Radix Sorts/Quick3string.java
@@ -34,7 +34,9 @@ public final class Quick3string {
      */
     private static int charAt(final String s, final int d) {
         assert d >= 0 && d <= s.length();
-        if (d == s.length()) return -1;
+        if (d == s.length()) {
+            return -1;
+        }
         return s.charAt(d);
     }
 
@@ -61,14 +63,22 @@ public final class Quick3string {
         int i = lo + 1;
         while (i <= gt) {
             int t = charAt(a[i], d);
-            if      (t < v) exch(a, lt++, i++);
-            else if (t > v) exch(a, i, gt--);
-            else              i++;
+            if (t < v) {
+                exch(a, lt++, i++);
+            }
+            else if (t > v) {
+                exch(a, i, gt--);
+            }
+            else {
+                i++;
+            }
         }
 
         // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi].
         sort(a, lo, lt - 1, d);
-        if (v >= 0) sort(a, lt, gt, d + 1);
+        if (v >= 0) {
+            sort(a, lt, gt, d + 1);
+        }
         sort(a, gt + 1, hi, d);
     }
 
@@ -82,9 +92,11 @@ public final class Quick3string {
      */
     private static void insertion(final String[] a, final int lo,
                                   final int hi, final int d) {
-        for (int i = lo; i <= hi; i++)
-            for (int j = i; j > lo && less(a[j], a[j - 1], d); j--)
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > lo && less(a[j], a[j - 1], d); j--) {
                 exch(a, j, j - 1);
+            }
+        }
     }
 
     /**
@@ -112,8 +124,12 @@ public final class Quick3string {
     private static boolean less(final String v, final String w, final int d) {
         assert v.substring(0, d).equals(w.substring(0, d));
         for (int i = d; i < Math.min(v.length(), w.length()); i++) {
-            if (v.charAt(i) < w.charAt(i)) return true;
-            if (v.charAt(i) > w.charAt(i)) return false;
+            if (v.charAt(i) < w.charAt(i)) {
+                return true;
+            }
+            if (v.charAt(i) > w.charAt(i)) {
+                return false;
+            }
         }
         return v.length() < w.length();
     }
@@ -126,9 +142,11 @@ public final class Quick3string {
      * @return     True if sorted, False otherwise.
      */
     private static boolean isSorted(String[] a) {
-        for (int i = 1; i < a.length; i++)
-            if (a[i].compareTo(a[i - 1]) < 0) return false;
+        for (int i = 1; i < a.length; i++) {
+            if (a[i].compareTo(a[i - 1]) < 0) {
+                return false;
+            }
+        }
         return true;
     }
-
 }
